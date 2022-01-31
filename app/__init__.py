b3d19dfc8c721354933d5cb76cc0cd3c3587dee4
Zarate Carreno Jose Valentin <lc19160007@oaxaca.tecnm.mx>
2022-01-31 00:27:17 -0600

    ✅ Adding: init

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,15 @@
 from flask import Flask
-
 app = Flask(__name__)
 
-from app.routes import  user
+
+from app.routes import user
+
+app.secret_key = 'super secret key'
+app.config['SESSION_TYPE'] = 'filesystem'
+app.jinja_env.auto_reload = True
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+    
+    
